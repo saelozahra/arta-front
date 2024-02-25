66e12a0cc6acf5755678a6f782927d5f5fc09d39
SaeloZahra <saelozahra14@gmail.com>
2024-02-25 13:28:42 +0330

    date picker from and to date

--- js/script.js
+++ js/script.js
@@ -31,10 +31,42 @@ $(document).ready(function ($) {
         });
     }
 
-    $(".dpex").pDatepicker({
-    observer: true,
-    format: 'dddd DD MMMM',
-});
+    var to, from;
+    to = $("#dp_to").persianDatepicker({
+        // inline: true,
+        // altField: '.range-to-example-alt',
+        format: 'dddd DD MMMM',
+        // altFormat: 'LLLL',
+        initialValue: false,
+        onSelect: function (unix) {
+            to.touched = true;
+            if (from && from.options && from.options.maxDate != unix) {
+                var cachedValue = from.getState().selected.unixDate;
+                from.options = {maxDate: unix};
+                if (from.touched) {
+                    from.setDate(cachedValue);
+                }
+            }
+        }
+    });
+
+    from = $("#dp_from").persianDatepicker({
+        // inline: true,
+        // altField: '.range-from-example-alt',
+        format: 'dddd DD MMMM',
+        // altFormat: 'LLLL',
+        initialValue: false,
+        onSelect: function (unix) {
+            from.touched = true;
+            if (to && to.options && to.options.minDate != unix) {
+                var cachedValue = to.getState().selected.unixDate;
+                to.options = {minDate: unix};
+                if (to.touched) {
+                    to.setDate(cachedValue);
+                }
+            }
+        }
+    });
 
 })
 
